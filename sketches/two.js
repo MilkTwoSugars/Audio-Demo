b8a0918f3a64bef49f7d52f1cc6322deb0b1ade8
MilkTwoSugars <amackey.mail@gmail.com>
2018-11-05 21:36:24 +0000

    Update sketches/two.js

--- sketches/two.js
+++ sketches/two.js
@@ -1,8 +1,6 @@
 class SketchTwo {
     constructor() {
-        this.modifier1 = 1;
-        this.modifier2 = 1;
-        this.modifier3 = 1;
+        this.modifier = 1;
         this.targetModifier = 1;
 
         this.colour = 255;
@@ -19,27 +17,26 @@ class SketchTwo {
         stroke(255);
         strokeWeight(1)
 
-        for (var i = 0; i < 6; i++) {
+        for (var i = 0; i < 4; i++) {
             push()
-            rotateX(frameCount * 0.005) + (i * this.modifier2)
-            rotateY(frameCount * 0.001 + (i * this.modifier1))
-            rotateZ(frameCount * 0.0013 + (i * this.modifier3))
-            stroke((this.colour / i), 100, 255);
-            sphere((this.s * i))
+            rotateX(frameCount * 0.005)
+            rotateY(frameCount * 0.01 + (i * this.modifier))
+            rotateZ(frameCount * 0.015)
+            stroke(this.colour / i, 255, 255);
+            translate(i * 10, 0)
+            sphere((this.s * 2) + sin(frameCount * 0.01), 3, 3)
             pop()
         }
 
-        this.modifier1 = lerp(this.modifier1, this.targetModifier, 0.07)
-        this.modifier2 = lerp(this.modifier1, this.targetModifier, 0.08)
-        this.modifier3 = lerp(this.modifier1, this.targetModifier, 0.09)
+        this.modifier = lerp(this.modifier, this.targetModifier, 0.1)
         this.colour = lerp(this.colour, this.targetColour, 0.1)
-        this.s = lerp(this.s, this.ts, 0.01)
+        this.s = lerp(this.s, this.ts, 0.1)
 
     }
 
-    tick() {
+    tick(){
         this.targetModifier += 0.05
         this.targetColour = random(255);
-        this.ts = random(150, 500)
+        this.ts = random(150, 250)
     }
 }
